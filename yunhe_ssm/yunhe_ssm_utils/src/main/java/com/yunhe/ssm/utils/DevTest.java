c1792e4ebffbbf37f7a59290559fca514aaabee0
James <zhanshixiang1997@163.com>
2019-08-10 15:52:00 +0800

    创建 DevTest测试dev分支类

---
+++ yunhe_ssm/yunhe_ssm_utils/src/main/java/com/yunhe/ssm/utils/DevTest.java
@@ -0,0 +1,14 @@
+package com.yunhe.ssm.utils;
+
+/**
+ * @author James
+ * @create 2019-08-10 15:50
+ */
+public class DevTest {
+
+
+     public static void main(String[] args){
+
+         System.out.println("测试dev分支");
+     }
+}
